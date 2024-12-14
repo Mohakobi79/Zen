72f26d853ceac77d1b8983f01ed3af697ea77f3e
Mohakobi79 <mohakobi79@gmail.com>
2024-12-14 20:34:38 +0100

    Update owner-setbotpp.js

--- plugins/owner-setbotpp.js
+++ plugins/owner-setbotpp.js
@@ -4,7 +4,7 @@ let handler = async (m, { conn, args }) => {
     let mime = (q.msg || q).mimetype || ''
     if (/image/.test(mime)) {
       let img = await q.download()
-      if (!img) throw `Fotonya Gak Ada Kak *┰ω┰*`
+      if (!img) throw `Fotonya Gak Ada Kk *┰ω┰*`
      conn.updateProfilePicture (bot, img)
     conn.reply(m.chat, 'Selesai Mengganti Profil Bot Kak*>ω<*!', m)
 	}
